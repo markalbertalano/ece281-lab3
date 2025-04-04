9805840dd36270de0ee78aede2d8236af58b4f9d
markalbertalano <markalbertalano@users.noreply.github.com>
2025-04-03 22:04:20 -0600

    tb.1

--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -57,28 +57,82 @@ end thunderbird_fsm_tb;
 architecture test_bench of thunderbird_fsm_tb is 
 	
 	component thunderbird_fsm is 
---	  port(
-		
---	  );
-	end component thunderbird_fsm;
+        port(
+            i_clk, i_reset  : in    std_logic;
+            i_left, i_right : in    std_logic;
+            o_lights_L      : out   std_logic_vector(2 downto 0);
+            o_lights_R      : out   std_logic_vector(2 downto 0)
+         );
+  
+	end component;
 
-	-- test I/O signals
+	-- test I signals
+	signal w_clk : std_logic := '0';
+	signal w_reset : std_logic := '0';
+	signal w_left : std_logic := '0';
+	signal w_right: std_logic := '0';
+	
+	-- test O signals
+    signal w_lights_L : std_logic_vector(2 downto 0) := "000";
+    signal w_lights_R : std_logic_vector(2 downto 0) := "000";
+	
+	-- test STATES signals
+    signal w_S : std_logic_vector(2 downto 0) := "000";
+	signal w_S_next : std_logic_vector(2 downto 0) := "000";
 	
 	-- constants
-	
+	constant k_clk_period : time := 10 ns;
 	
 begin
-	-- PORT MAPS ----------------------------------------
-	
+	-- PORT MAPS ---------------------------------------- based on ice4
+	uut: thunderbird_fsm port map (
+	       i_clk => w_clk,
+	       i_reset => w_reset,
+	       i_left => w_left,
+	       i_right => w_right,
+	       
+	       o_lights_L => w_lights_L,
+	       o_lights_R => w_lights_R
+	       
+	       --MIGHT BE A PROBLEM
+	       --f_S => w_S,
+	       --f_S_next => w_S_next
+    );
+	       
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
-    -- Clock process ------------------------------------
-    
+    -- Clock process ------------------------------------ from ice4
+	clk_proc : process
+	begin
+		w_clk <= '0';
+        wait for k_clk_period/2;
+		w_clk <= '1';
+		wait for k_clk_period/2;
+	end process;
+	
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
-	
+    sim_proc : process
+    begin
+        -- reset
+        w_reset <= '1';
+        wait for k_clk_period * 2;
+        assert (w_lights_L = "000") report "reset lights_L" severity error;
+        assert (w_lights_R = "000") report "reset lights_R" severity error;
+    
+    
+        -- transitions 
+        w_left <= '1';
+        w_right <= '1';
+        wait for k_clk_period * 2;
+        assert (w_lights_L = "100") report "transition lights_L" severity error;
+        assert (w_lights_R = "011") report "transition lights_R" severity error;
+    
+        wait;
+    end process;
+
 	-----------------------------------------------------	
 	
 end test_bench;
