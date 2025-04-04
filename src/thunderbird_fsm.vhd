af5804b10b0a6a8b06c520fcaeeddb3e011b1831
markalbertalano <markalbertalano@users.noreply.github.com>
2025-04-03 22:30:37 -0600

    tb.3

--- src/thunderbird_fsm.vhd
+++ src/thunderbird_fsm.vhd
@@ -107,7 +107,8 @@ begin
 	
 	-- next state --
 	f_S_next(0) <= ( (not f_S(2)) and (not f_S(1)) and (not f_S(0)) and i_left and i_right ) or 
-	               ( (f_S(1)) and (not f_S(0)) );
+	               ( (f_S(1)) and (not f_S(0)) ) or
+	               ( (f_S(2)) and (not f_S(1)) and (not f_S(0)) and i_left and i_right );
 	               
 	f_S_next(1) <= ( (not f_S(2)) and (not f_S(1)) and (not f_S(0)) and (not i_left) and i_right ) or 
 	               ( f_S(2) and (not f_S(1)) and f_S(0) ) or 
